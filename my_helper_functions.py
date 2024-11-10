6cedc22e2a1c1018eafbc04a21728b3dbfa6d444
smartful <remi_rodrigues@hotmail.com>
2024-11-10 07:17:03 +0100

    erase useless comments

--- my_helper_functions.py
+++ my_helper_functions.py
@@ -57,16 +57,6 @@ def accuracy_fn(y_pred, y_true):
 
 
 def print_train_time(start: float, end: float, device: torch.device = None):
-    """Prints difference between start and end time.
-
-    Args:
-        start (float): Start time of computation (preferred in timeit format). 
-        end (float): End time of computation.
-        device ([type], optional): Device that compute is running on. Defaults to None.
-
-    Returns:
-        float: time between start and end in seconds (higher is longer).
-    """
     total_time = end - start
     print(f"Train time on {device}: {total_time:.3f} seconds")
     return total_time
